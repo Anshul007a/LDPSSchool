0b9b8f7deb6614f9b5fd7089da495f123f801f5e
Anshul Asati <anshulasati@Anshuls-MacBook-Air.local>
2023-10-30 22:49:58 +0530

    ADDNEWDATA

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+
+  <SchoolMainpage />
+  <WebBody/>
+
+
+</div>
+
+</template>
+
+<script>
+
+import SchoolMainpage from './components/SchoolMainpage.vue'
+import WebBody from './components/WebBody.vue';
+export default {
+  name: 'App',
+  components: {
+
+    SchoolMainpage,
+    WebBody
+  }
+}
+</script>
+
+<style>
+#app {
+  
+}
+</style>
---
+++ src/components/ChildComponentcard.vue
@@ -0,0 +1,56 @@
+<template>
+ <div>
+    <div class="card-group">
+  <div class="card card-style">
+    <img src="../assets/principle.jpeg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card card-style">
+    <img src="../assets/playground.jpeg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card card-style">
+    <div class="card-body">
+        <div class="card m-2 mt-5">
+        <div class="card-body bg-primary text-white ">
+          <h1 class="card-title text-center ">Play School</h1>
+        </div>
+      </div>
+      <div class="card m-2">
+        <div class="card-body bg-success text-white">
+            <h1 class="card-title text-center ">Junior School</h1>
+        </div>
+      </div>
+      <div class="card m-2">
+        <div class="card-body bg-danger text-white">
+            <h1 class="card-title text-center ">Middle School</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+ </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.card-style{
+    margin: 20px;
+}
+
+</style>
